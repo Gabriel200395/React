13a5b1c09087b75ebf5d4b5e5a434cc05b77e1f7
Gabriel Souza <eugabrielf@gmail.com>
2022-12-08 19:11:57 -0300

    finalizando frontend

--- projetos/todo-list/src/page/App.tsx
+++ projetos/todo-list/src/page/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-         todo
-      </header>
+    <div className="page-container">
+      <div className="header-grid-page">
+        <div className="header-grid-title">
+          <h2>Minhas Tarefas</h2>
+        </div>
+      </div>
+
+      <div className="page-container-input">
+        <div className="page-grid-input">
+          <input type="text" placeholder="Adicionar  Uma Tarefa" />
+          <button>+</button>
+        </div>
+      </div>
+
+      <div className="page-container-todo-list">
+        <div className="page-grid-todo-list">
+          <div className="page-grid-todo-list-text">
+            <input type="checkbox" name="" id="" />
+            <p>Descrição</p>
+          </div>
+          <button>
+            <i className="fa fa-trash" aria-hidden="true" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
